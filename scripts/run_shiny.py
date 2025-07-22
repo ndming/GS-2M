d905823e08def7137f422361857e0c3894d23c35
ndming <ndminh1101@gmail.com>
2025-07-22 08:54:13 +0200

    Fix shiny

--- scripts/run_shiny.py
+++ scripts/run_shiny.py
@@ -1,4 +1,4 @@
-scenes = ["helmet", "car", "teapot", "coffee", "toaster", "ball"]
-ref_thresholds = [0.1, 0.5, 0.1, 0.2, 0.1]
-lambda_smooths = [0.5, 0.0, 0.5, 0.5, 0.5]
-lambda_normals = [2.5, 0.5, 0.1, 0.1, 4.0]
+scenes = ["helmet", "car", "teapot", "ball", "coffee", "toaster"]
+ref_thresholds = [0.1, 0.5, 0.1, 0.2, 0.2, 0.1]
+lambda_smooths = [0.5, 0.0, 0.5, 0.4, 0.5, 0.8]
+lambda_normals = [2.5, 0.5, 0.1, 5.0, 0.1, 4.0]
